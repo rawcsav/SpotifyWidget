f7e6e4fa7f8cde4a8b8fedae985f6fc75dace578
rawc <gmasontf2@gmail.com>
2024-05-28 17:44:07 -0400

    feat: integrate album cover color extraction in track info
    - Add  dependency to extract primary color from album cover
    - Extract primary color from album cover using  and include it in the track info response

--- api/spotify-now.js
+++ api/spotify-now.js
@@ -1,75 +1,88 @@
-const SpotifyWebApi = require('spotify-web-api-node');
+     const Vibrant = require('node-vibrant');
+     const SpotifyWebApi = require('spotify-web-api-node');
 
-const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    refreshToken: process.env.REFRESH_TOKEN,
-});
+     const spotifyApi = new SpotifyWebApi({
+         clientId: process.env.SPOTIFY_CLIENT_ID,
+         clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+         refreshToken: process.env.REFRESH_TOKEN,
+     });
 
-async function refreshAccessToken() {
-    try {
-        const data = await spotifyApi.refreshAccessToken();
-        const {access_token} = data.body;
-        spotifyApi.setAccessToken(access_token);
-    } catch (error) {
-        console.error('Could not refresh access token', error);
-    }
-}
+     async function refreshAccessToken() {
+         try {
+             const data = await spotifyApi.refreshAccessToken();
+             const { access_token } = data.body;
+             spotifyApi.setAccessToken(access_token);
+         } catch (error) {
+             console.error('Could not refresh access token', error);
+         }
+     }
 
-async function getTrackInfo() {
-    try {
-        await refreshAccessToken();
-        const currentTrack = await spotifyApi.getMyCurrentPlayingTrack();
+     async function getTrackInfo() {
+         try {
+             await refreshAccessToken();
+             const currentTrack = await spotifyApi.getMyCurrentPlayingTrack();
 
-        if (currentTrack.body && currentTrack.body.is_playing) {
-            const trackInfo = {
-                artist: currentTrack.body.item.artists.map((artist) => artist.name).join(', '),
-                artist_urls: currentTrack.body.item.artists.map((artist) => `https://open.spotify.com/artist/${artist.id}`),
-                track_name: currentTrack.body.item.name,
-                album_cover: currentTrack.body.item.album.images[1].url,
-                song_url: `https://open.spotify.com/track/${currentTrack.body.item.id}`,
-                playing: true,
-                track_id: currentTrack.body.item.id,
-            };
-            return trackInfo;
-        } else {
-            const recentlyPlayed = await spotifyApi.getMyRecentlyPlayedTracks({limit: 1});
+             if (currentTrack.body && currentTrack.body.is_playing) {
+                 const trackInfo = {
+                     artist: currentTrack.body.item.artists.map((artist) => artist.name).join(', '),
+                     artist_urls: currentTrack.body.item.artists.map((artist) => `https://open.spotify.com/artist/${artist.id}`),
+                     track_name: currentTrack.body.item.name,
+                     album_cover: currentTrack.body.item.album.images[1].url,
+                     song_url: `https://open.spotify.com/track/${currentTrack.body.item.id}`,
+                     playing: true,
+                     track_id: currentTrack.body.item.id,
+                 };
+                 return trackInfo;
+             } else {
+                 const recentlyPlayed = await spotifyApi.getMyRecentlyPlayedTracks({ limit: 1 });
 
-            if (recentlyPlayed.body && recentlyPlayed.body.items.length > 0) {
-                const lastTrack = recentlyPlayed.body.items[0].track;
-                const trackInfo = {
-                    artist: lastTrack.artists.map(artist => artist.name).join(', '),
-                    artist_urls: lastTrack.artists.map((artist) => `https://open.spotify.com/artist/${artist.id}`),
-                    track_name: lastTrack.name,
-                    album_cover: lastTrack.album.images[1].url,
-                    song_url: `https://open.spotify.com/track/${lastTrack.id}`,
-                    playing: false,
-                    track_id: lastTrack.id,
-                };
-                return trackInfo;
-            } else {
-                throw new Error('No track information available or not playing');
-            }
-        }
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
-    }
-}
+                 if (recentlyPlayed.body && recentlyPlayed.body.items.length > 0) {
+                     const lastTrack = recentlyPlayed.body.items[0].track;
+                     const trackInfo = {
+                         artist: lastTrack.artists.map(artist => artist.name).join(', '),
+                         artist_urls: lastTrack.artists.map((artist) => `https://open.spotify.com/artist/${artist.id}`),
+                         track_name: lastTrack.name,
+                         album_cover: lastTrack.album.images[1].url,
+                         song_url: `https://open.spotify.com/track/${lastTrack.id}`,
+                         playing: false,
+                         track_id: lastTrack.id,
+                     };
+                     return trackInfo;
+                 } else {
+                     throw new Error('No track information available or not playing');
+                 }
+             }
+         } catch (error) {
+             console.error('Error fetching data:', error);
+             throw error;
+         }
+     }
 
-module.exports = async (req, res) => {
-    try {
-        // Check the Origin header of the request
-        const origin = req.headers.origin;
+     module.exports = async (req, res) => {
+         try {
+             // Check the Origin header of the request
+             const origin = req.headers.origin;
 
-        // Allow CORS requests only from localhost and rawcsav.com
-        if (origin && (origin.includes('localhost') || origin.includes('rawcsav.com'))) {
-            res.setHeader('Access-Control-Allow-Origin', origin);
-        }
+             // Allow CORS requests only from localhost and rawcsav.com
+             if (origin && (origin.includes('localhost') || origin.includes('rawcsav.com'))) {
+                 res.setHeader('Access-Control-Allow-Origin', origin);
+             }
 
-        const trackInfo = await getTrackInfo();
-        res.json(trackInfo);
-    } catch (error) {
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-};
+             const trackInfo = await getTrackInfo();
+
+             // Extract primary color from album cover using Vibrant
+             Vibrant.from(trackInfo.album_cover).getPalette((err, palette) => {
+                 if (err) {
+                     console.error('Error extracting color:', err);
+                     res.json(trackInfo); // Send track info without primary color if extraction fails
+                 } else {
+                     const primaryColor = palette.Vibrant.getRgb();
+                     trackInfo.primary_color = `rgb(${primaryColor[0]}, ${primaryColor[1]}, ${primaryColor[2]})`;
+                     res.json(trackInfo);
+                 }
+             });
+
+         } catch (error) {
+             res.status(500).json({ error: 'Internal Server Error' });
+         }
+     };
